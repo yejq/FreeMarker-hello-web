b6c0427daa4773347c26760da7f1f890509ca2b1
jim.ye <jim.ye@corp.globalmarket.com>
2014-09-10 12:20:49 +0800

    init

---
+++ src/main/java/org/yejq/fre/controller/TestController.java
@@ -0,0 +1,29 @@
+package org.yejq.fre.controller;
+
+import java.lang.reflect.Method;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.yejq.fre.service.Exercises;
+
+@Controller
+public class TestController {
+
+	//http://localhost/test/4/if/testIf
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@RequestMapping("/test/{folder}/{file}/{methodName}")
+	public String test(@PathVariable("folder") String folder, @PathVariable("file") String file,
+			@PathVariable("methodName") String methodName, Model model) throws Exception {
+		
+		if (StringUtils.isNotBlank(methodName) && !"null".equals(methodName)) {
+			Exercises exercises = new Exercises();
+			Class clazz = exercises.getClass();
+			Method method = clazz.getMethod(methodName, Model.class);
+			method.invoke(exercises, model);
+		}
+		return folder + "/" + file;
+	}
+}
---
+++ src/main/java/org/yejq/fre/service/Exercises.java
@@ -0,0 +1,32 @@
+package org.yejq.fre.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.ui.Model;
+import org.yejq.fre.model.Animal;
+
+public class Exercises{
+	
+	public void testIf(Model model){
+		model.addAttribute("user", "Big Joe");
+		
+		Map<String, Animal> animals = new HashMap<String, Animal>();
+		animals.put("python", new Animal("python", 300, true));
+		animals.put("elephant", new Animal("elephant", 400, false));
+		model.addAttribute("animals", animals);
+	}
+	
+	public void testList(Model model){
+		List<Animal> animals = new ArrayList<Animal>();
+		animals.add(new Animal("python", 300, true));
+		animals.add(new Animal("elephant", 400, false));
+		model.addAttribute("animals", animals);
+	}
+	
+	public void testMethod(Model model){
+		model.addAttribute("indexOf", new IndexOfMethod());
+	}
+}
